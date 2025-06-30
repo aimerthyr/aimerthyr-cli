b96c1be7fa3f4343c96d9a2a4cfbcac57a93fd38
aimerthyr <aimerthy@foxmail.com>
2025-06-30 23:42:19 +0800

    chore: 修改包名

--- bin/index.js
+++ bin/index.js
@@ -1,23 +1,20 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-import { createProject } from '../commands/create/index.js';
+import { Command } from "commander";
+import { createProject } from "../commands/create/index.js";
 
 const program = new Command();
 
-program
-  .name('chao-cli')
-  .description('前端项目脚手架')
-  .version('1.0.0');
+program.name("aimerthyr-cli").description("前端项目脚手架").version("1.0.0");
 
 // create 命令（目前支持了创建命令，后续可新增其他命令）
 program
-  .command('create')
-  .description('创建一个新项目')
-  .argument('[projectName]', '项目名称')
+  .command("create")
+  .description("创建一个新项目")
+  .argument("[projectName]", "项目名称")
   .action((projectName) => {
     if (!projectName) {
-      console.log('\n❌ 项目名未填写，请使用：');
-      console.log('   chao-cli create <project-name>\n');
+      console.log("\n❌ 项目名未填写，请使用：");
+      console.log("   aimerthyr-cli create <project-name>\n");
       process.exit(1);
     }
 
